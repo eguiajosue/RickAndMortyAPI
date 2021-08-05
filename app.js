3b6bda3b48b50969f9c090b50f10821b57c65cab
eguiajosue <eguiajosue9@gmail.com>
2021-08-05 00:40:37 -0500

    Search function added

--- app.js
+++ app.js
@@ -1,27 +1,74 @@
-characterContainer = document.querySelector('.container');
+const element = document.querySelector("form");
+element.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Form submission cancelled.");
+});
+
+characterContainer = document.querySelector(".container");
+btnSearch = document.querySelector(".search-btn");
+searchInput = document.querySelector(".search-input");
+
+btnSearch.addEventListener("click", (e) => {
+  fetch(`https://rickandmortyapi.com/api/character/?name=${searchInput.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      characterContainer.innerHTML = data.results.map(
+        (char) => `
+
+          <div class="character-card">
+            <h3 class="character-name">${char.name}</h3>
+            <img class="character-img" src="${char.image}">
+            <span class="character-data">${char.species}</span>
+            <span class="character-data">${char.status}</span>
+            <span class="character-data">${char.origin.name}</span>
+          </div>
+          
+        `
+      );
+    });
+});
 
 // Print default characters
 fetch(`https://rickandmortyapi.com/api/character/?page=1`)
   .then((res) => res.json())
   .then((data) => {
-      characterContainer.innerHTML = data.results.map(char => `
-      <h3>${char.name}</h3>
-      `);
+    characterContainer.innerHTML = data.results.map(
+      (char) => `
+
+      <div class="character-card">
+        <h3 class="character-name">${char.name}</h3>
+        <img class="character-img" src="${char.image}">
+        <span class="character-data">${char.species}</span>
+        <span class="character-data">${char.status}</span>
+        <span class="character-data">${char.origin.name}</span
+      </div>
+      
+      `
+    );
   });
 
 //! Navigation Code
 let page = 1;
 const prevPage = document.querySelector(".n-prev");
 const fowardPage = document.querySelector(".n-foward");
-
 // Previous page
 prevPage.addEventListener("click", (e) => {
   fetch(`https://rickandmortyapi.com/api/character/?page=${--page}`)
     .then((res) => res.json())
     .then((data) => {
-        characterContainer.innerHTML = data.results.map(char => `
-        <h3>${char.name}</h3>
-      `);
+      characterContainer.innerHTML = data.results.map(
+        (char) => `
+
+        <div class="character-card">
+            <h3 class="character-name">${char.name}</h3>
+            <img class="character-img" src="${char.image}">
+            <span class="character-data">${char.species}</span>
+            <span class="character-data">${char.status}</span>
+            <span class="character-data">${char.origin.name}</span
+        </div>
+        
+      `
+      );
     });
 });
 
@@ -30,9 +77,18 @@ fowardPage.addEventListener("click", (e) => {
   fetch(`https://rickandmortyapi.com/api/character/?page=${++page}`)
     .then((res) => res.json())
     .then((data) => {
-        characterContainer.innerHTML = data.results.map(char => `
-        <h3>${char.name}</h3>
-      `);
+      characterContainer.innerHTML = data.results.map(
+        (char) => `
+
+        <div class="character-card">
+            <h3 class="character-name">${char.name}</h3>
+            <img class="character-img" src="${char.image}">
+            <span class="character-data">${char.species}</span>
+            <span class="character-data">${char.status}</span>
+            <span class="character-data">${char.origin.name}</span
+        </div>
+        
+      `
+      );
     });
 });
-
