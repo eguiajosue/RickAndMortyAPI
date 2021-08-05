a4dd7831111aed0cf9d412d29122391a5538f5a3
eguiajosue <eguiajosue9@gmail.com>
2021-08-05 16:06:55 -0500

    css update

--- js/app.js
+++ js/app.js
@@ -7,3 +7,5 @@ fetch("https://rickandmortyapi.com/api/character/?page=1")
   .then((data) => {
     characterContainer.innerHTML = data.results.map((char) => drawCharacter(char)).join("");
   });
+
+  
