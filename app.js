ef5d8988f70ed58e23e3e427486feb3af724ccdd
eguiajosue <eguiajosue9@gmail.com>
2021-08-04 23:43:05 -0500

    navigation

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-search = document.querySelector('.input-id');
-button = document.querySelector('.btn-search');
 characterContainer = document.querySelector('.container');
 
+// Print default characters
+fetch(`https://rickandmortyapi.com/api/character/?page=1`)
+  .then((res) => res.json())
+  .then((data) => {
+      characterContainer.innerHTML = data.results.map(char => `
+      <h3>${char.name}</h3>
+      `);
+  });
 
+//! Navigation Code
+let page = 1;
+const prevPage = document.querySelector(".n-prev");
+const fowardPage = document.querySelector(".n-foward");
 
-function takeCharacters () {
-    fetch('https://rickandmortyapi.com/api/character/1')
-    .then(res => res.json())
-    .then(data => console.log(data))
-}
+// Previous page
+prevPage.addEventListener("click", (e) => {
+  fetch(`https://rickandmortyapi.com/api/character/?page=${--page}`)
+    .then((res) => res.json())
+    .then((data) => {
+        characterContainer.innerHTML = data.results.map(char => `
+        <h3>${char.name}</h3>
+      `);
+    });
+});
 
-takeCharacters();
+// Next page
+fowardPage.addEventListener("click", (e) => {
+  fetch(`https://rickandmortyapi.com/api/character/?page=${++page}`)
+    .then((res) => res.json())
+    .then((data) => {
+        characterContainer.innerHTML = data.results.map(char => `
+        <h3>${char.name}</h3>
+      `);
+    });
+});
+
